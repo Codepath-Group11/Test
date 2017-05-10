0b485177cdfdfab9e51c037c5d73e481973e598e
Arthur Burgin <arb4778@rit.edu>
2017-05-10 12:47:18 -0400

    start work for saving settings for playlist

---
+++ MyMusic/PlaylistSettingsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  PlaylistSettingsViewController.swift
+//  MyMusic
+//
+//  Created by Arthur Burgin on 5/10/17.
+//  Copyright © 2017 Yerneni, Naresh. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistSettingsViewController: UIViewController {
+    @IBOutlet var moodSegmentedControl: UISegmentedControl!
+    @IBOutlet var energySegmentedControl: UISegmentedControl!
+    @IBOutlet var danceSegmentedControl: UISegmentedControl!
+    @IBOutlet var tempoSegmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveSettingsTapped(_ sender: UIButton) {
+        let parseObj = saveSelectedSettings()
+        for (k, v) in parseObj.enumerated(){
+            print("\(k): \(v)")
+        }
+        //save into parse database
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func dismissSettingsTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func saveSelectedSettings()->[String: Int]{
+        var selectedSettings: [String: Int] = [:]
+        
+        selectedSettings["mood"] = moodSegmentedControl.selectedSegmentIndex
+        selectedSettings["energy"] = energySegmentedControl.selectedSegmentIndex
+        selectedSettings["dance"] = danceSegmentedControl.selectedSegmentIndex
+        selectedSettings["tempo"] = tempoSegmentedControl.selectedSegmentIndex
+        
+        return selectedSettings
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
